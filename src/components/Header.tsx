bc1ac6e773db6ada751427d29aadd6e92dd71dc4
CristianLeyton <leytoncristian96@gmail.com>
2025-11-13 11:25:21 -0300

    metadatos

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+    return (
+        <header className="p-4 flex justify-around font-semibold">
+
+            <nav className="flex gap-2">
+            <a href="#">Bienvenida</a>
+            <a href="#">Reuniones</a>
+            <a href="#">Conectar</a>
+            <a href="#">Actividades</a>
+            <a href="#">Contribuir</a>
+            </nav>
+        </header>
+    )
+};
+
